7f4973bb77e95d7bdf41a26d387e39cd9b73cdba
Ollie Buck <github@olliebuck.com>
2012-01-04 12:23:16 +0000

    add some tests, including for datetimes with DST boundary overlap

--- test.js
+++ test.js
@@ -30,5 +30,20 @@ test('calculator', {
   'Ensure error is thrown when one of the dates is not supplied' : function() {
     var err = calculator.calculate('12/12/2011');
     assert.ok(/InvalidParams: Missing a date parameter/.test(err.message));
+  },
+
+  'Check dates + times are supported': function() {
+    assert.equal(calculator.calculate('2012-01-01 1200', '2012-01-02 1400'), 1);
+  },
+
+  'Check datetimes crossing a DST boundary are supported': function() {
+    // 25th march 2012 -> 1hr
+    assert.equal(calculator.calculate('2012-03-24 1200', '2012-03-26 1300'), 2);
+  },
+
+  'Check date object support': function() {
+    var a = new Date(2012, 02, 02);
+    var b = new Date(2012, 02, 05);
+    assert.equal(calculator.calculate(a, b), 3);
   }
 });
