f6b585d3009112bc38ebb1b1a3c04ce3675a5f31
Ollie Buck <github@olliebuck.com>
2012-01-11 12:05:27 +0000

    add support for datetimes + convenience methods to get durations in whole numbers of days

--- test.js
+++ test.js
@@ -33,12 +33,29 @@ test('calculator', {
   },
 
   'Check dates + times are supported': function() {
-    assert.equal(calculator.calculate('2012-01-01 1200', '2012-01-02 1400'), 1);
+    assert.equal(calculator.calculate('2012-01-01 12:00', '2012-01-02 12:00'), 1);
   },
 
-  'Check datetimes crossing a DST boundary are supported': function() {
+  'Check calculate returns an exact difference': function() {
+    assert.equal(calculator.calculate('2012-01-01 12:00', '2012-01-02 13:00'), (25/24));
+  },
+
+  'Check calculateWholeDays returns a whole number with excess hours ignored': function() {
+    assert.equal(calculator.calculateWholeDays('2012-01-01 12:00', '2012-01-03 13:00'), 2);
+  },
+
+  'Check calculatePartialDays returns a whole number with excess hours counting as a whole day': function() {
+    assert.equal(calculator.calculatePartialDays('2012-01-01 12:00', '2012-01-03 13:00'), 3);
+  },
+
+  'Check datetimes crossing a DST boundary one way are supported': function() {
     // 25th march 2012 -> 1hr
-    assert.equal(calculator.calculate('2012-03-24 1200', '2012-03-26 1300'), 2);
+    assert.equal(calculator.calculate('2012-03-24 12:00', '2012-03-26 13:00'), 2);
+  },
+
+  'Check datetimes crossing a DST boundary the other way are supported': function() {
+    // 25th march 2012 -> 1hr
+    assert.equal(calculator.calculate('2012-10-27 13:00', '2012-10-29 12:00'), 2);
   },
 
   'Check date object support': function() {
